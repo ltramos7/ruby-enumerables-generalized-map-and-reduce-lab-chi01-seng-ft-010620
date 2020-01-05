c9e60a8d29edab35a1f208c1447d39d8467fb762
Linda Ramos <ltramos7@gmail.com>
2020-01-05 22:06:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -2,3 +2,17 @@
 # def map (array) 
   
 # end
+
+def reduce (list,sv=nil)
+  sum = 0
+  i = 0
+  if (sv)
+    sum = sv
+  end
+
+  while i < list.length
+    sum = sum + list[i]
+    i += 1
+  end
+  sum
+end
