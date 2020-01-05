5ef46d956ca6912d5131642fd388593784094638
Linda Ramos <ltramos7@gmail.com>
2020-01-05 22:52:51 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,30 @@
 # Your Code Here
+def map (array) 
+  i=0
+  new_array = []
+  while i < array.length
+    new_array << yield(array[i]) 
+    i += 1 
+  end
+  new_array
+end
+
+
+
+
+def reduce (list,sv=nil)
+
+  if (sv)
+    sum = sv
+    i=0
+  else
+    sum = list[0]
+    i=1
+  end
+
+  while i < list.length
+    sum = yield(sum, list[i])
+    i += 1
+  end
+  sum
+end
